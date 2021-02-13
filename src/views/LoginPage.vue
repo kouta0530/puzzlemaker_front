53a2d6ccfa3d631fa0b2a1c5c0f812b66422c379
kouta0530 <16jk047@ymklab.net>
2021-02-14 03:06:28 +0900

    feat(login) loginpageの追加

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <p-form formLabel="ログイン" @sendForm="login"> </p-form>
+</template>
+<script>
+import { Vue, Component } from "vue-property-decorator";
+import PForm from "@/components/PForm";
+import { UserRegistrationData } from "@/models/UserRegistrationData";
+
+@Component({
+  components: {
+    PForm,
+  },
+})
+export default class LoginPage extends Vue {
+  login() {
+    console.log("get");
+  }
+}
+</script>
+<style scoped></style>
